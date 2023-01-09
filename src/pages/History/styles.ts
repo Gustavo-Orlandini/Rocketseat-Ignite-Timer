4c82e27fe3a9863660b9c7cd41f44e77f3847813
*Gustavo <guga77sugu@hotmail.com>
2023-01-09 11:40:04 -0300

    include color status ball

--- src/pages/History/styles.ts
+++ src/pages/History/styles.ts
@@ -60,3 +60,28 @@ export const HistoryList = styled.div`
     }
   }
 `
+
+// aqui vai ser criado a bolinha do status com suas respectivas cores!
+const STATUS_COLORS = {
+  yellow: 'yellow-500',
+  green: 'green-500',
+  red: 'red-500',
+} as const
+
+interface StatusPorps {
+  statusColor: keyof typeof STATUS_COLORS
+}
+
+export const Status = styled.span<StatusPorps>`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  &::before {
+    content: '';
+    width: 0.5rem;
+    height: 0.5rem;
+    border-radius: 50%;
+    background: ${(props) => props.theme[STATUS_COLORS[props.statusColor]]};
+  }
+`
