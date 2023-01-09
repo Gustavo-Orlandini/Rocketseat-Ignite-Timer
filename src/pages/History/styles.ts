aafc195f88e96ae2e07a2834b15ce1167c52d1e6
*Gustavo <guga77sugu@hotmail.com>
2023-01-09 10:33:31 -0300

    Create history page

---
+++ src/pages/History/styles.ts
@@ -0,0 +1,62 @@
+import styled from 'styled-components'
+
+export const HistoryContainer = styled.main`
+  flex: 1;
+  padding: 3.5rem;
+  display: flex;
+  flex-direction: column;
+
+  h1 {
+    font-size: 1.5rem;
+    color: ${(props) => props.theme['gray-100']};
+  }
+`
+
+// Essa div abaixo foi criada para ajudar na versão moble...
+// Pois quando o usuário estiver no celular, vai poder dar scroll
+export const HistoryList = styled.div`
+  flex: 1;
+  overflow: auto;
+  margin-top: 2rem;
+
+  table {
+    width: 100%;
+    border-collapse: collapse; //aqui ele não soma o pixel da borda no tamanho do conteúdo
+    min-width: 600px; //aqui ele forçará a criar o scroll em telas pequenas
+  }
+
+  th {
+    background-color: ${(props) => props.theme['gray-600']};
+    padding: 1rem;
+    text-align: left;
+    color: ${(props) => props.theme['gray-100']};
+    font-size: 0.875rem;
+    line-height: 1.6;
+
+    &:first-child {
+      border-top-left-radius: 8px;
+      padding-left: 1.5rem;
+    }
+
+    &:last-child {
+      border-top-right-radius: 8px;
+      padding-right: 1.5rem;
+    }
+  }
+  td {
+    background-color: ${(props) => props.theme['gray-700']};
+    border-top: 4px solid ${(props) => props.theme['gray-800']};
+    padding: 1rem;
+    font-size: 0.875rem;
+    line-height: 1.6;
+
+    &:first-child {
+      width: 50%;
+      padding-left: 1.5rem;
+    }
+
+    &:last-child {
+      padding-right: 1.5rem;
+    }
+  }
+`
