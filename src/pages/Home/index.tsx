8c5287116fc83ffc428b0ee1632c4737c2748c2a
*Gustavo <guga77sugu@hotmail.com>
2023-01-10 16:01:30 -0300

    Finished Home components and resolve bugs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -33,15 +33,20 @@ export function Home() {
       minutesAmount: 0,
     },
   })
-  // reset
-  const { handleSubmit, watch } = newCycleForm
+
+  const { handleSubmit, watch, reset } = newCycleForm
+
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    createNewCycle(data)
+    reset()
+  }
 
   const task = watch('task')
   const isSubmitDisabled = !task
 
   return (
     <HomeContainer>
-      <form onSubmit={handleSubmit(createNewCycle)} action="">
+      <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
         <FormProvider {...newCycleForm}>
           <NewCycleForm />
         </FormProvider>
